9d66125c635b7f89cb8903e64fa0e013870ef3b5
theLivin <ayiteybobby@gmail.com>
2020-03-18 20:13:34 -0400

    Create grid menu

---
+++ ce_youthful_app/lib/boxes.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+class Boxes extends StatelessWidget {
+  BoxDecoration _myBoxDecoration(Color myColor) {
+    return BoxDecoration(
+      color: myColor,
+      borderRadius: BorderRadius.circular(20.0),
+    );
+  }
+
+  TextStyle _topRowTextStyle(double myFontSize, Color myColor) {
+    return TextStyle(
+      fontSize: myFontSize,
+      color: myColor,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.italic,
+    );
+  }
+
+  TextStyle _middleRowTextStyle() {
+    return TextStyle(
+      fontSize: 40,
+      color: Colors.white,
+      fontWeight: FontWeight.bold,
+      fontStyle: FontStyle.italic,
+    );
+  }
+
+  TextStyle _bottomRowTextStyle() {
+    return TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          // top row
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: _myBoxDecoration(Colors.blue),
+                child: Center(
+                  child: Text(
+                    "SUPER\nSUNDAY",
+                    style: _topRowTextStyle(30, Colors.white),
+                  ),
+                ),
+              ),
+              Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: _myBoxDecoration(Colors.indigo),
+                child: Center(
+                  child: Text(
+                    'MIDWEEK',
+                    style: _topRowTextStyle(20, Colors.black),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // middle row
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                height: 200.0,
+                width: 350.0,
+                decoration: _myBoxDecoration(Colors.purple),
+                child: Center(
+                  child: Text(
+                    'SWAG SERVICE',
+                    style: _middleRowTextStyle(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // bottom row
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: _myBoxDecoration(Colors.red),
+                child: Center(
+                  child: Text(
+                    '#EVENTFLYER',
+                    style: _bottomRowTextStyle(),
+                  ),
+                ),
+              ),
+              Container(
+                height: 150.0,
+                width: 150.0,
+                decoration: _myBoxDecoration(Colors.yellow),
+                child: Center(
+                  child: Text(
+                    '#EVENTFLYER',
+                    style: _bottomRowTextStyle(),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
